6e829b3f885f308369528e880030d96b256f0d69
Abdullah272056 <abdullah272056@gmail.com>
2020-11-09 20:39:04 +0600

    LoginActivity login Function add

--- app/src/main/java/com/example/pointtosellpractice/LoginActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/LoginActivity.java
@@ -5,12 +5,23 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.pointtosellpractice.model_class.LogInData;
+import com.example.pointtosellpractice.model_class.RegistrationData;
+import com.example.pointtosellpractice.retrofit.ApiInterface;
+import com.example.pointtosellpractice.retrofit.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
 
@@ -18,6 +29,8 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     Button signInButton;
     TextView signUpTextView,resetPasswordTextView;
     ProgressBar progressBar;
+
+    ApiInterface apiInterface,apiInterface1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +50,10 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         signInButton.setOnClickListener(this);
         signUpTextView.setOnClickListener(this);
         resetPasswordTextView.setOnClickListener(this);
+
+        //initialize apiInterface
+        apiInterface = RetrofitClient.getRetrofit("http://mern-pos.herokuapp.com/").create(ApiInterface.class);
+
 
     }
 
@@ -79,5 +96,25 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             signInPasswordEditText.requestFocus();
             return;
         }
+
+        LogInData logInData       =new LogInData(signInEmail,signInPassword);
+
+        apiInterface.logInData(logInData).enqueue(new Callback<LogInData>() {
+            @Override
+            public void onResponse(Call<LogInData> call, Response<LogInData> response) {
+                if (response.isSuccessful()){
+                    Toast.makeText(LoginActivity.this, "success", Toast.LENGTH_SHORT).show();
+                    Log.e("TAG", "success");
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LogInData> call, Throwable t) {
+                Log.e("wwe",t.getMessage().toString());
+                Toast.makeText(LoginActivity.this, "fail", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
     }
 }
