5e2b7dff87dc4c68d8b77dc6bf91f124791e234b
Abdullah272056 <abdullah272056@gmail.com>
2020-11-09 20:26:58 +0600

    view finding

--- app/src/main/java/com/example/pointtosellpractice/LoginActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/LoginActivity.java
@@ -3,12 +3,33 @@ package com.example.pointtosellpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText signInEmailEditText,signInPasswordEditText;
+    Button signInButton;
+    TextView signUpTextView,resetPasswordTextView;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //view finding
+        signInEmailEditText=findViewById(R.id.signInEmailEditTextId);
+        signInPasswordEditText=findViewById(R.id.signInPasswordEditTextId);
+        signInButton=findViewById(R.id.signInButtonId);
+        signUpTextView=findViewById(R.id.signUpTextViewId);
+        resetPasswordTextView=findViewById(R.id.resetPasswordTextViewId);
+        progressBar=findViewById(R.id.signInProgressBarId);
+
+
+
+
     }
 }
