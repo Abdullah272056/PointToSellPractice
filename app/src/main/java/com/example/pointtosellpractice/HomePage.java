d569ab6c2db9eb15c000e8fcc07ccfa813e040d1
Abdullah272056 <abdullah272056@gmail.com>
2020-11-10 22:31:25 +0600

    HomePage.java file view finding

--- app/src/main/java/com/example/pointtosellpractice/HomePage.java
+++ app/src/main/java/com/example/pointtosellpractice/HomePage.java
@@ -1,17 +1,35 @@
 package com.example.pointtosellpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.LinearLayout;
+
+import com.google.android.material.navigation.NavigationView;
 
 public class HomePage extends AppCompatActivity {
-    WebView myWebView;
+    Toolbar toolbar;
+    LinearLayout linearLayout;
+    DrawerLayout drawerLayout;
+    NavigationView navigationView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home_page);
+
+        linearLayout=findViewById(R.id.mainLayoutId);
+        toolbar=findViewById (R.id.toolbarId);
+        if (toolbar!=null){
+            setSupportActionBar (toolbar);
+        }
+
+        drawerLayout=findViewById (R.id.drawerLayoutId);
+        navigationView=findViewById (R.id.myNavigationViewId);
+        
 
     }
 }
