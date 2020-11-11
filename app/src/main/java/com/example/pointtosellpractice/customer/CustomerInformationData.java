1d452434798d975c8ffd6d410736612cfb45e6ba
Abdullah272056 <abdullah272056@gmail.com>
2020-11-11 10:26:44 +0600

    CustomerInformationData class update

--- app/src/main/java/com/example/pointtosellpractice/customer/CustomerInformationData.java
+++ app/src/main/java/com/example/pointtosellpractice/customer/CustomerInformationData.java
@@ -1,4 +1,40 @@
 package com.example.pointtosellpractice.customer;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 public class CustomerInformationData {
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("address")
+    @Expose
+    private String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
