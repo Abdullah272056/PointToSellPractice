bab74685cfdc6dad6190b00fb6d6d1e58b2433ee
Abdullah272056 <abdullah272056@gmail.com>
2020-11-11 16:26:14 +0600

    view finding

--- app/src/main/java/com/example/pointtosellpractice/CustomerActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/CustomerActivity.java
@@ -1,14 +1,23 @@
 package com.example.pointtosellpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class CustomerActivity extends AppCompatActivity {
+    RecyclerView customerRecyclerView;
+    FloatingActionButton addCustomerButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer);
+        customerRecyclerView=findViewById(R.id.customerRecyclerViewId);
+        addCustomerButton=findViewById(R.id.addCustomerButtonId);
+
+        
     }
 }
