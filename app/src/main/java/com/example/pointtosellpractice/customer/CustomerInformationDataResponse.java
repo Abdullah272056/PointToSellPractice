bf2eb206fc10c9e3caa572d1086400319a76069b
Abdullah272056 <abdullah272056@gmail.com>
2020-11-11 13:31:49 +0600

    CustomerInformationDataResponse class update

--- app/src/main/java/com/example/pointtosellpractice/customer/CustomerInformationDataResponse.java
+++ app/src/main/java/com/example/pointtosellpractice/customer/CustomerInformationDataResponse.java
@@ -1,4 +1,42 @@
 package com.example.pointtosellpractice.customer;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class CustomerInformationDataResponse {
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("customer")
+    @Expose
+    private List<CustomerInformationData> customerInformation = null;
+    @SerializedName("msg")
+    @Expose
+    private String msg;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public List<CustomerInformationData> getCustomerInformation() {
+        return customerInformation;
+    }
+
+    public void setCustomerInformation(List<CustomerInformationData> customerInformation) {
+        this.customerInformation = customerInformation;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
