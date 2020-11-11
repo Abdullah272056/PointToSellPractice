7b7b0558d9656280fe24af8c94ace0382e1d1ca6
Abdullah272056 <abdullah272056@gmail.com>
2020-11-11 19:58:50 +0600

    create constructor in CustomerCustomAdapter

--- app/src/main/java/com/example/pointtosellpractice/customer/CustomerCustomAdapter.java
+++ app/src/main/java/com/example/pointtosellpractice/customer/CustomerCustomAdapter.java
@@ -1,12 +1,23 @@
 package com.example.pointtosellpractice.customer;
 
+import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.List;
+
 public class CustomerCustomAdapter extends RecyclerView.Adapter<CustomerCustomAdapter.MyViewHolder> {
+  List<CustomerInformationDataResponse> customerInformationDataResponseList;
+    Context context;
+
+    public CustomerCustomAdapter(List<CustomerInformationDataResponse> customerInformationDataResponseList, Context context) {
+        this.customerInformationDataResponseList = customerInformationDataResponseList;
+        this.context = context;
+    }
+
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
