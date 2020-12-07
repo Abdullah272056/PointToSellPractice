846d2f0cfb4ee96d0da465dc466638e2bfcb7484
Abdullah272056 <abdullah272056@gmail.com>
2020-12-07 19:19:21 +0600

    create UpComingFeature.java and design

---
+++ app/src/main/java/com/example/pointtosellpractice/UpComingFeature.java
@@ -0,0 +1,14 @@
+package com.example.pointtosellpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UpComingFeature extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_up_coming_feature);
+    }
+}
