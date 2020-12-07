9e2719725688984f9fb8053d53db55e8090fb138
Abdullah272056 <abdullah272056@gmail.com>
2020-12-07 23:19:43 +0600

    create Customer.java

---
+++ app/src/main/java/com/example/pointtosellpractice/model_class/invoice/get_all_invoice/Customer.java
@@ -0,0 +1,52 @@
+package com.example.pointtosellpractice.model_class.invoice.get_all_invoice;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Customer {
+
+    @SerializedName("due")
+    @Expose
+    private Integer due;
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("address")
+    @Expose
+    private String address;
+    @SerializedName("name")
+    @Expose
+    private String name;
+
+    public Integer getDue() {
+        return due;
+    }
+
+    public void setDue(Integer due) {
+        this.due = due;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
