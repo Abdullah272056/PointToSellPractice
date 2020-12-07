94c0d8f25534883c99824828cd1d3ce83b0d494b
Abdullah272056 <abdullah272056@gmail.com>
2020-12-07 16:04:01 +0600

    write code in GetAllSellInfoResponse class

--- app/src/main/java/com/example/pointtosellpractice/model_class/invoice/GetAllSellInfoResponse.java
+++ app/src/main/java/com/example/pointtosellpractice/model_class/invoice/GetAllSellInfoResponse.java
@@ -5,5 +5,38 @@ import com.google.gson.annotations.SerializedName;
 
 public class GetAllSellInfoResponse {
 
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("totalSaleInfo")
+    @Expose
+    private GetAllSellInfoData getAllSellInfoData;
+    @SerializedName("msg")
+    @Expose
+    private String msg;
 
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public GetAllSellInfoData getGetAllSellInfoData() {
+        return getAllSellInfoData;
+    }
+
+    public void setGetAllSellInfoData(GetAllSellInfoData getAllSellInfoData) {
+        this.getAllSellInfoData = getAllSellInfoData;
+    }
+
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 }
