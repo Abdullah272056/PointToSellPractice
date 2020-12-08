c40fb6db7e27d00562d2f03aed705c2395c50eb1
Abdullah272056 <abdullah272056@gmail.com>
2020-12-08 15:20:42 +0600

    Create InVoiceDetails.java

---
+++ app/src/main/java/com/example/pointtosellpractice/InVoiceDetails.java
@@ -0,0 +1,14 @@
+package com.example.pointtosellpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InVoiceDetails extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_voice_details);
+    }
+}
