24a57a8cefc4925970d8e295800567ddad9c2333
Abdullah272056 <abdullah272056@gmail.com>
2020-12-09 11:56:33 +0600

    create GetProductData.java and add code

---
+++ app/src/main/java/com/example/pointtosellpractice/model_class/product/GetProductData.java
@@ -0,0 +1,162 @@
+package com.example.pointtosellpractice.model_class.product;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class GetProductData {
+
+    @SerializedName("price")
+    @Expose
+    private Integer price;
+    @SerializedName("sellingPrice")
+    @Expose
+    private Integer sellingPrice;
+    @SerializedName("stock")
+    @Expose
+    private Integer stock;
+    @SerializedName("quantity")
+    @Expose
+    private Integer quantity;
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("unit")
+    @Expose
+    private String unit;
+    @SerializedName("description")
+    @Expose
+    private String description;
+    @SerializedName("user")
+    @Expose
+    private String user;
+    @SerializedName("image")
+    @Expose
+    private String image;
+    @SerializedName("cloudinary_id")
+    @Expose
+    private String cloudinaryId;
+    @SerializedName("createdAt")
+    @Expose
+    private String createdAt;
+    @SerializedName("updatedAt")
+    @Expose
+    private String updatedAt;
+    @SerializedName("__v")
+    @Expose
+    private Integer v;
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Integer sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public Integer getStock() {
+        return stock;
+    }
+
+    public void setStock(Integer stock) {
+        this.stock = stock;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getUser() {
+        return user;
+    }
+
+    public void setUser(String user) {
+        this.user = user;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getCloudinaryId() {
+        return cloudinaryId;
+    }
+
+    public void setCloudinaryId(String cloudinaryId) {
+        this.cloudinaryId = cloudinaryId;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public String getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(String updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Integer getV() {
+        return v;
+    }
+
+    public void setV(Integer v) {
+        this.v = v;
+    }
+}
