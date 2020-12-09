15ba022df9fc4660dd717778221dbc9ff40cc5bf
Abdullah272056 <abdullah272056@gmail.com>
2020-12-09 11:58:20 +0600

    create GetProductDataResponse.java and add code

---
+++ app/src/main/java/com/example/pointtosellpractice/model_class/product/GetProductDataResponse.java
@@ -0,0 +1,42 @@
+package com.example.pointtosellpractice.model_class.product;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class GetProductDataResponse {
+    @SerializedName("success")
+    @Expose
+    private Boolean success;
+    @SerializedName("products")
+    @Expose
+    private List<GetProductData> products = null;
+    @SerializedName("msg")
+    @Expose
+    private String msg;
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public List<GetProductData> getProducts() {
+        return products;
+    }
+
+    public void setProducts(List<GetProductData> products) {
+        this.products = products;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+}
