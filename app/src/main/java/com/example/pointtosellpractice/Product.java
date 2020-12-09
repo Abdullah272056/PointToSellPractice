eda24e98849206e3bb438b3c69a2c4c2e273f79d
Abdullah272056 <abdullah272056@gmail.com>
2020-12-09 13:45:17 +0600

    add product data show by recyclerview function

--- app/src/main/java/com/example/pointtosellpractice/Product.java
+++ app/src/main/java/com/example/pointtosellpractice/Product.java
@@ -26,15 +26,49 @@ import retrofit2.Callback;
 import retrofit2.Response;
 
 public class Product extends AppCompatActivity {
-
+    List<GetProductData> getProductDataList;
+    String token;
+    ApiInterface apiInterface;
+    ProductCustomAdapter productCustomAdapter;
+    RecyclerView productRecyclerView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product);
+        token= getIntent().getStringExtra("token");
+        apiInterface = RetrofitClient.getRetrofit("http://mern-pos.herokuapp.com/").create(ApiInterface.class);
+        productRecyclerView=findViewById(R.id.productRecyclerViewId);
 
+        getAllProduct();
 
 
     }
-    
+
+    public void getAllProduct() {
+        apiInterface.getAllProduct("Bearer "+token).enqueue(new Callback<GetProductDataResponse>() {
+            @Override
+            public void onResponse(Call<GetProductDataResponse> call, Response<GetProductDataResponse> response) {
+                GetProductDataResponse getProductDataResponse=response.body();
+
+                if (getProductDataResponse.getSuccess()==true){
+                    getProductDataList=new ArrayList<>();
+                    getProductDataList.addAll(response.body().getProducts());
+                    Toast.makeText(Product.this, String.valueOf(getProductDataList.size()), Toast.LENGTH_SHORT).show();
+                    if (getProductDataList.size ()>0){
+                        productCustomAdapter = new ProductCustomAdapter(Product.this,token,getProductDataList);
+                        productRecyclerView.setLayoutManager(new LinearLayoutManager(Product.this));
+                        productRecyclerView.setAdapter(productCustomAdapter);
+                    }
+            }
+            }
+
+            @Override
+            public void onFailure(Call<GetProductDataResponse> call, Throwable t) {
+
+            }
+        });
+
+
+    }
 
 }
