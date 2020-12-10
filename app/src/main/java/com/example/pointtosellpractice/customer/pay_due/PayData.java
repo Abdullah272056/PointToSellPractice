cbeede0af3f8e6f25f602e8f693a2c4766c64872
Abdullah272056 <abdullah272056@gmail.com>
2020-12-10 22:13:28 +0600

    create PayData.java

---
+++ app/src/main/java/com/example/pointtosellpractice/customer/pay_due/PayData.java
@@ -0,0 +1,29 @@
+package com.example.pointtosellpractice.customer.pay_due;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class PayData {
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("payAmount")
+    @Expose
+    private Integer payAmount;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Integer getPayAmount() {
+        return payAmount;
+    }
+
+    public void setPayAmount(Integer payAmount) {
+        this.payAmount = payAmount;
+    }
+}
