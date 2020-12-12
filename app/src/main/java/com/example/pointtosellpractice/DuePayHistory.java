da7f98ae05187e666d62f54128f462d555370d2a
Abdullah272056 <abdullah272056@gmail.com>
2020-12-12 23:54:34 +0600

    create DuePayHistory.java activity

---
+++ app/src/main/java/com/example/pointtosellpractice/DuePayHistory.java
@@ -0,0 +1,16 @@
+package com.example.pointtosellpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DuePayHistory extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_due_pay_history);
+
+
+    }
+}
