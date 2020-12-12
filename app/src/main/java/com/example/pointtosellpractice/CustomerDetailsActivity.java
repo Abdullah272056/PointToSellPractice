a780b35675fc415fa65294a7185db315ff730d7d
Abdullah272056 <abdullah272056@gmail.com>
2020-12-13 00:36:02 +0600

    remove some code

--- app/src/main/java/com/example/pointtosellpractice/CustomerDetailsActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/CustomerDetailsActivity.java
@@ -1,36 +1,18 @@
 package com.example.pointtosellpractice;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.example.pointtosellpractice.customer.CustomerCustomAdapter;
-import com.example.pointtosellpractice.customer.CustomerInformationData;
-import com.example.pointtosellpractice.customer.CustomerInformationDataResponse;
-import com.example.pointtosellpractice.customer.pay_due.DuePayData;
 import com.example.pointtosellpractice.customer.pay_due.DuePayDataResponse;
 import com.example.pointtosellpractice.customer.pay_due.PayData;
-import com.example.pointtosellpractice.customer.single_customer.SingleCustomerDuePayCustomAdapter;
-import com.example.pointtosellpractice.customer.single_customer.SingleCustomerDuePayHistory;
-import com.example.pointtosellpractice.customer.single_customer.SingleCustomerGetResponse;
-import com.example.pointtosellpractice.model_class.LogInData;
 import com.example.pointtosellpractice.retrofit.ApiInterface;
 import com.example.pointtosellpractice.retrofit.RetrofitClient;
-
-import java.util.ArrayList;
-import java.util.List;
-
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
