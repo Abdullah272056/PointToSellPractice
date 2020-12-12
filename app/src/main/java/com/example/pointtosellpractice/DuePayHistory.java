3ac3563edbd565838d00bbea37c4dffd4acb263c
Abdullah272056 <abdullah272056@gmail.com>
2020-12-13 00:22:41 +0600

    receive data

--- app/src/main/java/com/example/pointtosellpractice/DuePayHistory.java
+++ app/src/main/java/com/example/pointtosellpractice/DuePayHistory.java
@@ -8,12 +8,13 @@ import android.os.Bundle;
 import com.example.pointtosellpractice.customer.single_customer.SingleCustomerDuePayCustomAdapter;
 
 public class DuePayHistory extends AppCompatActivity {
- 
+ String customer_id,token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_due_pay_history);
-
+        customer_id= getIntent().getStringExtra("customerId");
+        token=getIntent().getStringExtra("token");
 
     }
 }
