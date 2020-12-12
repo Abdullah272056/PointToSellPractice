f13b1087bda176a010f0e94358db935cfe1befb9
Abdullah272056 <abdullah272056@gmail.com>
2020-12-13 01:05:04 +0600

    create SingleCustomerTotalSellActivity.java

---
+++ app/src/main/java/com/example/pointtosellpractice/SingleCustomerTotalSellActivity.java
@@ -0,0 +1,14 @@
+package com.example.pointtosellpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SingleCustomerTotalSellActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_single_customer_total_sell);
+    }
+}
