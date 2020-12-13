5c1013e3318755d6ad43c3cd240e83ec5e055435
Abdullah272056 <abdullah272056@gmail.com>
2020-12-13 15:41:16 +0600

    receive data

--- app/src/main/java/com/example/pointtosellpractice/SingleCustomerTotalSellActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/SingleCustomerTotalSellActivity.java
@@ -13,11 +13,16 @@ import com.example.pointtosellpractice.retrofit.ApiInterface;
 import java.util.List;
 
 public class SingleCustomerTotalSellActivity extends AppCompatActivity {
-  
+    String customer_id,token;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_single_customer_total_sell);
+
+        //receive data
+        customer_id= getIntent().getStringExtra("customerId");
+        token=getIntent().getStringExtra("token");
+
     }
 }
