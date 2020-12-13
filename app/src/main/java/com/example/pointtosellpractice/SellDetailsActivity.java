d0d42c7b7b729783bc31c5a49ca69ac3cc04f8a9
Abdullah272056 <abdullah272056@gmail.com>
2020-12-13 21:54:39 +0600

    data receive

--- app/src/main/java/com/example/pointtosellpractice/SellDetailsActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/SellDetailsActivity.java
@@ -2,13 +2,16 @@ package com.example.pointtosellpractice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class SellDetailsActivity extends AppCompatActivity {
-
+int position;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_details);
+        position=getIntent().getIntExtra("position",10000);
+
     }
 }
