217eb4e54980479275dd5c459f48943a70952902
Abdullah272056 <abdullah272056@gmail.com>
2020-12-13 21:04:05 +0600

    extend RecyclerView.Adapter in SingleCustomerSellsDetailsCustomAdapter.java

--- app/src/main/java/com/example/pointtosellpractice/customer/single_customer/SingleCustomerSellsDetailsCustomAdapter.java
+++ app/src/main/java/com/example/pointtosellpractice/customer/single_customer/SingleCustomerSellsDetailsCustomAdapter.java
@@ -1,5 +1,32 @@
 package com.example.pointtosellpractice.customer.single_customer;
 
-public class SingleCustomerSellsDetailsCustomAdapter {
-    
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SingleCustomerSellsDetailsCustomAdapter extends RecyclerView.Adapter<SingleCustomerSellsDetailsCustomAdapter.MyViewHolder> {
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
 }
