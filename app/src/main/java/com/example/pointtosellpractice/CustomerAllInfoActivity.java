c25f94f99565022dc4e3e3014bccf01d88fdc4aa
Abdullah272056 <abdullah272056@gmail.com>
2020-12-15 07:45:02 +0600

    create CustomerAllInfoActivity.java activity

---
+++ app/src/main/java/com/example/pointtosellpractice/CustomerAllInfoActivity.java
@@ -0,0 +1,14 @@
+package com.example.pointtosellpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CustomerAllInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_customer_all_info);
+    }
+}
