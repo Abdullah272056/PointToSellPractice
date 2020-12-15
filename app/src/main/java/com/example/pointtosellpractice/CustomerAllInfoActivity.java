98600995714288f16e01b2f218819072495678c5
Abdullah272056 <abdullah272056@gmail.com>
2020-12-15 08:09:38 +0600

    textView finding

--- app/src/main/java/com/example/pointtosellpractice/CustomerAllInfoActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/CustomerAllInfoActivity.java
@@ -3,12 +3,25 @@ package com.example.pointtosellpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class CustomerAllInfoActivity extends AppCompatActivity {
-
+        TextView customerNameTextView,customerDataTextView,customerPayDueTextView,
+                customerDuePayHistoryTextView,customerTotalSellTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_all_info);
+
+        //textView Finding
+        customerNameTextView=findViewById(R.id.customerNameTextViewId);
+        customerDataTextView=findViewById(R.id.customerDataTextViewId);
+        customerPayDueTextView=findViewById(R.id.customerPayDueTextViewId);
+        customerDuePayHistoryTextView=findViewById(R.id.customerDuePayHistoryTextViewId);
+        customerTotalSellTextView=findViewById(R.id.customerTotalSellTextViewId);
+
+
     }
 }
