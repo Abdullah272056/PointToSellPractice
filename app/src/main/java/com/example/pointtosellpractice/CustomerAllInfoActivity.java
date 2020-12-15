6e8d039c1b684447fb10a71358b1c51d5d49815f
Abdullah272056 <abdullah272056@gmail.com>
2020-12-15 08:11:42 +0600

    set onclickListener with textView

--- app/src/main/java/com/example/pointtosellpractice/CustomerAllInfoActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/CustomerAllInfoActivity.java
@@ -3,6 +3,7 @@ package com.example.pointtosellpractice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -21,6 +22,32 @@ public class CustomerAllInfoActivity extends AppCompatActivity {
         customerPayDueTextView=findViewById(R.id.customerPayDueTextViewId);
         customerDuePayHistoryTextView=findViewById(R.id.customerDuePayHistoryTextViewId);
         customerTotalSellTextView=findViewById(R.id.customerTotalSellTextViewId);
+
+        customerDataTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        customerPayDueTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        customerDuePayHistoryTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        customerTotalSellTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
 
 
     }
