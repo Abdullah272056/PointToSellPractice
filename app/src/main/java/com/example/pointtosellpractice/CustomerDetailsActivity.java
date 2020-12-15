4dca318000cf70dc3263d028b9db51ad83def9f3
Abdullah272056 <abdullah272056@gmail.com>
2020-12-15 09:34:44 +0600

    some code remove from CustomerDetailsActivity.java

--- app/src/main/java/com/example/pointtosellpractice/CustomerDetailsActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/CustomerDetailsActivity.java
@@ -1,11 +1,9 @@
 package com.example.pointtosellpractice;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,21 +12,16 @@ import android.widget.TextView;
 import android.widget.Toast;
 import com.example.pointtosellpractice.customer.pay_due.DuePayDataResponse;
 import com.example.pointtosellpractice.customer.pay_due.PayData;
-import com.example.pointtosellpractice.customer.single_customer.SingleCustomerDuePayCustomAdapter;
 import com.example.pointtosellpractice.customer.single_customer.SingleCustomerGetResponse;
-import com.example.pointtosellpractice.customer.single_customer.SingleCustomerInformation;
 import com.example.pointtosellpractice.retrofit.ApiInterface;
 import com.example.pointtosellpractice.retrofit.RetrofitClient;
 
-import java.util.ArrayList;
-import java.util.Collections;
 
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
 
 public class CustomerDetailsActivity extends AppCompatActivity {
-    Button duePayHistoryButton,singleCustomerTotalSellButton;
     ApiInterface apiInterface;
     ProgressBar pauDueProgressBar;
 
@@ -56,35 +49,18 @@ public class CustomerDetailsActivity extends AppCompatActivity {
         allTimeSellTextView=findViewById(R.id.allTimeSellTextViewId);
         //button finding
         payDueButton=findViewById(R.id.payDueButtonId);
-        duePayHistoryButton=findViewById(R.id.duePayHistoryButtonId);
-        singleCustomerTotalSellButton=findViewById(R.id.singleCustomerTotalSellButtonId);
+
         //editText finding
         duePayAmountEditText=findViewById(R.id.duePayAmountEditTextId);
 
         pauDueProgressBar=findViewById(R.id.pauDueProgressBarId);
-
-
-        // textView set Text
-//        cNameTextView.setText("Name :  "+getIntent().getStringExtra("cName"));
-//        cPhoneTextView.setText("Phone :  "+getIntent().getStringExtra("cPhone"));
-//        cEmailTextView.setText("Email :  "+getIntent().getStringExtra("cEmail"));
-//        cAddressTextView.setText("Address :  "+getIntent().getStringExtra("cAddress"));
-        //dueTextView.setText("Due :  "+getIntent().getStringExtra("cDue"));
+        //data receive
         customer_id=getIntent().getStringExtra("customerId");
         token= getIntent().getStringExtra("token");
 
         apiInterface = RetrofitClient.getRetrofit("http://mern-pos.herokuapp.com/").create(ApiInterface.class);
 
         customerInformation();
-        duePayHistoryButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(CustomerDetailsActivity.this, DuePayHistory.class);
-                intent.putExtra("token",token);
-                intent.putExtra("customerId",customer_id);
-                startActivity(intent);
-            }
-        });
 
         payDueButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -92,19 +68,8 @@ public class CustomerDetailsActivity extends AppCompatActivity {
                 payDue();
                 }
         });
-        singleCustomerTotalSellButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent=new Intent(CustomerDetailsActivity.this,SingleCustomerTotalSellActivity.class);
-                intent.putExtra("token",token);
-                intent.putExtra("customerId",customer_id);
-                startActivity(intent);
-                }
-        });
-
 
     }
-
 
     public void payDue(){
         duePayAmount = duePayAmountEditText.getText().toString();
@@ -127,7 +92,6 @@ public class CustomerDetailsActivity extends AppCompatActivity {
                     public void onResponse(Call<DuePayDataResponse> call, Response<DuePayDataResponse> response) {
                         assert response.body() != null;
                         if (response.body().getSuccess()){
-
                             Toast.makeText(CustomerDetailsActivity.this,response.body().getMsg() , Toast.LENGTH_SHORT).show();
                             duePayDataResponse=response.body();
                             //assert duePayDataResponse != null;
