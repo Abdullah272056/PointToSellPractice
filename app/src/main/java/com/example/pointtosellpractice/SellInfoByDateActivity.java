9d57deb4b87573b3473d7862282b2114511d6648
Abdullah272056 <abdullah272056@gmail.com>
2020-12-16 21:05:35 +0600

    receive data

--- app/src/main/java/com/example/pointtosellpractice/SellInfoByDateActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/SellInfoByDateActivity.java
@@ -5,10 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class SellInfoByDateActivity extends AppCompatActivity {
-
+String token;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_info_by_date);
+        //receive data
+        token= getIntent().getStringExtra("token");
+
     }
 }
