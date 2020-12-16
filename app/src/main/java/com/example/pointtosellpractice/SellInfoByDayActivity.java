9851c000fe369f35b7f5dac551ca645d8482eae6
Abdullah272056 <abdullah272056@gmail.com>
2020-12-17 01:47:31 +0600

    progress bar

--- app/src/main/java/com/example/pointtosellpractice/SellInfoByDayActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/SellInfoByDayActivity.java
@@ -8,6 +8,7 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -29,10 +30,13 @@ public class SellInfoByDayActivity extends AppCompatActivity {
     EditText sellInfoByDayEditText;
     TextView sellProductTextView,saleAmountTextView,profitTxtView,
             currentCashTextView,dueTextView,inVoiceTextView,productCostTextView;
+    ProgressBar sellInfoByDayProgressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sell_info_by_day);
+        //finding progressBar
+        sellInfoByDayProgressBar=findViewById(R.id.sellInfoByDayProgressBarId);
         //finding view
         sellInfoByDayOkButton=findViewById(R.id.sellInfoByDayOkButtonId);
         sellInfoByDayEditText=findViewById(R.id.sellInfoByDayEditTextId);
@@ -67,13 +71,13 @@ public class SellInfoByDayActivity extends AppCompatActivity {
             sellInfoByDayEditText.requestFocus();
             return;
         }
+        sellInfoByDayProgressBar.setVisibility(View.VISIBLE);
 
         apiInterface.getSellInfoByDay("Bearer "+token,Integer.valueOf(day)).enqueue(new Callback<GetSellInfoByDayResponse>() {
             @Override
             public void onResponse(Call<GetSellInfoByDayResponse> call, Response<GetSellInfoByDayResponse> response) {
                 if (response.body().getSuccess()==true){
                     GetSellInfoByDays getSellInfoByDays= response.body().getGetSellInfoByDays();
-//                    
                     sellProductTextView.setText(String.valueOf(getSellInfoByDays.getTotalSoldProduct()));
                     saleAmountTextView.setText(String.valueOf(getSellInfoByDays.getTotalSaleAmount()));
                     profitTxtView.setText(String.valueOf(getSellInfoByDays.getTotalProfit()));
@@ -81,13 +85,13 @@ public class SellInfoByDayActivity extends AppCompatActivity {
                     dueTextView.setText(String.valueOf(getSellInfoByDays.getTotalDue()));
                     inVoiceTextView.setText(String.valueOf(getSellInfoByDays.getTotalSoldInvoice()));
                     productCostTextView.setText(String.valueOf(getSellInfoByDays.getTotalProductCost()));
-
+                    sellInfoByDayProgressBar.setVisibility(View.INVISIBLE);
                     Toast.makeText(SellInfoByDayActivity.this, String.valueOf(response.body().getGetSellInfoByDays().getTotalSoldProduct()), Toast.LENGTH_SHORT).show();
                 }
             }
             @Override
             public void onFailure(Call<GetSellInfoByDayResponse> call, Throwable t) {
-
+                sellInfoByDayProgressBar.setVisibility(View.INVISIBLE);
                 sellProductTextView.setText("00");
                 saleAmountTextView.setText("00");
                 profitTxtView.setText("00");
