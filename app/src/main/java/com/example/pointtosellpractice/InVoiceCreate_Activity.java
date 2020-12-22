59bd1ffd1a0eee93708f60620a1b8fe1db944f4b
Abdullah272056 <abdullah272056@gmail.com>
2020-12-22 18:50:25 +0600

    create InVoiceCreate_Activity

---
+++ app/src/main/java/com/example/pointtosellpractice/InVoiceCreate_Activity.java
@@ -0,0 +1,14 @@
+package com.example.pointtosellpractice;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class InVoiceCreate_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_in_voice_create_);
+    }
+}
