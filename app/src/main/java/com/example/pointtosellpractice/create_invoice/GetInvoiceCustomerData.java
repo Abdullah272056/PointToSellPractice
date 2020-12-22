8875e78f8c410afa72ad935bea7c64e0f6e1e4aa
Abdullah272056 <abdullah272056@gmail.com>
2020-12-22 22:10:12 +0600

    create GetInvoiceCustomerData.java and add code

---
+++ app/src/main/java/com/example/pointtosellpractice/create_invoice/GetInvoiceCustomerData.java
@@ -0,0 +1,75 @@
+package com.example.pointtosellpractice.create_invoice;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class GetInvoiceCustomerData {
+
+
+    @SerializedName("due")
+    @Expose
+    private Integer due;
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("phone")
+    @Expose
+    private Integer phone;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("address")
+    @Expose
+    private String address;
+
+    public Integer getDue() {
+        return due;
+    }
+
+    public void setDue(Integer due) {
+        this.due = due;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPhone() {
+        return phone;
+    }
+
+    public void setPhone(Integer phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+}
