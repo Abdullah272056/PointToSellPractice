0160530fee223fe1bde71ace2975f17d03a97b62
Abdullah272056 <abdullah272056@gmail.com>
2020-12-25 12:01:10 +0600

    change SingleCustomerTotalSellActivity.java class's title

--- app/src/main/java/com/example/pointtosellpractice/SingleCustomerTotalSellActivity.java
+++ app/src/main/java/com/example/pointtosellpractice/SingleCustomerTotalSellActivity.java
@@ -40,6 +40,8 @@ public class SingleCustomerTotalSellActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        this.setTitle("Single customer total sell");
+
         setContentView(R.layout.activity_single_customer_total_sell);
 
         //receive data
