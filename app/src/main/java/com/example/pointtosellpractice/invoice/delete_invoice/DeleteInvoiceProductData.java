5c8f10956a9e697530a0ee2281e94a45e16ff010
Abdullah272056 <abdullah272056@gmail.com>
2021-01-10 23:16:38 +0600

    write code in DeleteInvoiceProductData.java

--- app/src/main/java/com/example/pointtosellpractice/invoice/delete_invoice/DeleteInvoiceProductData.java
+++ app/src/main/java/com/example/pointtosellpractice/invoice/delete_invoice/DeleteInvoiceProductData.java
@@ -1,5 +1,84 @@
 package com.example.pointtosellpractice.invoice.delete_invoice;
 
-public class DeleteInvoiceProductData {
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
 
+public class DeleteInvoiceProductData {
+    @SerializedName("_id")
+    @Expose
+    private String id;
+    @SerializedName("productId")
+    @Expose
+    private String productId;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("unit")
+    @Expose
+    private String unit;
+    @SerializedName("price")
+    @Expose
+    private Integer price;
+    @SerializedName("sellingPrice")
+    @Expose
+    private Integer sellingPrice;
+    @SerializedName("quantity")
+    @Expose
+    private Integer quantity;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public Integer getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(Integer sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
+
+    public Integer getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Integer quantity) {
+        this.quantity = quantity;
+    }
 }
